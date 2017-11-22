c44c49700ed8e9218ec3e96759ee71a442188233
dianamcnulty <dianamcnulty@gmail.com>
2017-11-22 11:11:15 -0500

    Create working for vacation
    fixed syntax for 'has_many' in vacation serializer

--- app/serializers/vacation_serializer.rb
+++ app/serializers/vacation_serializer.rb
@@ -1,5 +1,6 @@
 class VacationSerializer < ActiveModel::Serializer
   attributes :id, :country, :state, :month, :year, :companions
   has_one :user
-  has_many :photos, :stories
+  has_many :photos
+  has_many :stories
 end
