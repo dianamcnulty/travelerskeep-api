d4b39a17f65ff565d357e1c879ca97ee157120eb
dianamcnulty <dianamcnulty@gmail.com>
2017-11-23 21:33:09 -0500

    removes unnecessary methods for countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,7 +3,7 @@ class CountriesController < ApplicationController
 
   # GET /countries
   def index
-    @countries = Country.all
+    @countries = Country.all.order('name ASC')
 
     render json: @countries
   end
@@ -11,31 +11,6 @@ class CountriesController < ApplicationController
   # GET /countries/1
   def show
     render json: @country
-  end
-
-  # POST /countries
-  def create
-    @country = Country.new(country_params)
-
-    if @country.save
-      render json: @country, status: :created, location: @country
-    else
-      render json: @country.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /countries/1
-  def update
-    if @country.update(country_params)
-      render json: @country
-    else
-      render json: @country.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /countries/1
-  def destroy
-    @country.destroy
   end
 
   private
