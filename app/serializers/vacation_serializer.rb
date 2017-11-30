2414e2dee0183ce5c925fdae7f4f2ca6d89e364d
dianamcnulty <dianamcnulty@gmail.com>
2017-11-29 20:21:31 -0500

    add photo stuff

--- app/serializers/vacation_serializer.rb
+++ app/serializers/vacation_serializer.rb
@@ -1,6 +1,6 @@
 class VacationSerializer < ActiveModel::Serializer
   attributes :id, :country, :state, :month, :year, :companions
   has_one :user
-  # has_many :photos
+  has_many :photos
   has_many :stories
 end
